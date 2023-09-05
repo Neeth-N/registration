366bf8b662a660b5e8292d0bf6066b71df863321
Neeth N <116897540+Neeth-N@users.noreply.github.com>
2023-09-05 23:02:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyvKwbnTuSfooGynsxh9N7-XogJl_PHI8O5Er4mIVf8O8GgkeleyJvyYYhyw9wO29YwRg/exec'
+const form = document.forms['regisform']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(() => {  window.location.href = "submitted.html"; })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+
+        
